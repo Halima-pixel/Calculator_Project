7746f8e461ade8691e0a99773185b4389e2b7e13
halima-pixel <sannianuoluwapo610@gmail.com>
2024-09-04 15:56:27 +0100

    Created Functions for The Operations Found on a Calculator

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// The Calculator is going to contain functions for all basic math operations that can be found on a calculator
+// Start by creating functions for:
+/*add
+subtract
+multiply
+divide*/
+
+const add = (a, b) => {
+    if (isNaN(a) || isNaN(b)) {
+     throw new Error("input a valid number");
+    } 
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error("input a valid number");
+    }
+    return a -b;
+}
+
+const multiply = (a, b) => {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error("input a valid number");
+    }
+    return a * b;
+}
+
+const divide = (a, b) => {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error("input a valid number")
+    } else if ( b === 0 ) {
+        throw new Error("cannot Divide by 0");
+    }
+    return a / b;
+}
